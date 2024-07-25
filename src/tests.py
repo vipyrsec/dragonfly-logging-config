1ce2f9567731b21ccbfcea753b542ef2782ff587
AbooMinister25 <aboominister@gmail.com>
2024-07-25 13:50:23 -0400

    Add parameter to configure_logger for passing additional processors for configuring the logger

--- src/tests.py
+++ src/tests.py
@@ -1,6 +1,8 @@
 import io
+import logging
 import structlog
 import unittest
+from typing import Any
 
 from logging_config import configure_logger
 
@@ -50,3 +52,43 @@ class TestLogging(unittest.TestCase):
                 "func_name": "test_log_output",
             },
         )
+
+    def test_additional_processors(self):
+        """
+        Test whether passing additional processors works correctly.
+        """
+
+        f = io.StringIO()
+
+        config = {
+            "disable_existing_loggers": False,
+            "handlers": {
+                "default": {
+                    "level": "DEBUG",
+                    "class": "logging.StreamHandler",
+                    "stream": f,
+                    "formatter": "json",
+                },
+            },
+            "loggers": {
+                "": {"handlers": ["default"], "level": "DEBUG", "propagate": True},
+            },
+        }
+
+        def additional_processor(
+            logger: logging.Logger, method_name: str, event_dict: dict[str, Any]
+        ) -> dict[str, Any]:
+            """Add request id to log message."""
+            event_dict["testing"] = "testing"
+            return event_dict
+
+        configure_logger(config, additional_processors=[additional_processor])
+
+        cf = structlog.testing.CapturingLoggerFactory()
+        structlog.configure(logger_factory=cf)
+        log = structlog.get_logger()
+
+        log.info("test")
+        record = cf.logger.calls[0]
+
+        self.assertEqual(record.args[0]["testing"], "testing")
